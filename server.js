fc80ce9b464e425245852c96cc32afd566821a98
joemarygot <joemary@n-compass.biz>
2024-12-16 14:08:44 +0800

    Done Node.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+//Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); //Directory
+
+// console.log(__filename, __dirname);
+
+const server = http.createServer(async (req, res) => {
   //This line is what type of content you want in website
   //res.setHeader("Content-type", "text/html");
   //You can add status if it is an error
@@ -12,16 +21,25 @@ const server = http.createServer((req, res) => {
 
   try {
     if (req.method === "GET") {
+      let filePath;
       if (req.url === "/") {
-        res.writeHead(200, { "Content-type": "text/html" });
-        res.end("<h1>Homepage</h1>");
+        filePath = path.join(__dirname, "public", "index.html");
+        // res.writeHead(200, { "Content-type": "text/html" });
+        // res.end("<h1>Homepage</h1>");
       } else if (req.url === "/about") {
-        res.writeHead(200, { "Content-type": "text/html" });
-        res.end("<h1>About</h1>");
+        filePath = path.join(__dirname, "public", "about.html");
+
+        // res.writeHead(200, { "Content-type": "text/html" });
+        // res.end("<h1>About</h1>");
       } else {
-        res.writeHead(404, { "Content-type": "text/html" });
-        res.end("<h1>Not found</h1>");
+        throw new Error("Not Found");
+        // res.writeHead(404, { "Content-type": "text/html" });
+        // res.end("<h1>Not found</h1>");
       }
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
     } else {
       throw new Error("Method not allowed");
     }
@@ -37,3 +55,5 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+//fs - File System
